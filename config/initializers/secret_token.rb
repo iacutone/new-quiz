8ccbd6fd45f0e8e93c889fa34bb7e57fe06979ad
Eric Iacutone <eric.iacutone@gmail.com>
2013-07-23 00:37:41 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewQuiz::Application.config.secret_token = '6e895314c25afcbe0a5aa7b8eb73b1e5ea67cf863149bcbe9251c6f0856bb075ac674b57915439f15ff39b18cdffadda349e0cecd5b088dfb7b8ce07be7d729d'
