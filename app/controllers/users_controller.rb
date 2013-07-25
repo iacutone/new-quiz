e7d8e83a7f419d4ccdfe17fa75466941a258947d
Eric Iacutone <eric.iacutone@gmail.com>
2013-07-25 16:37:19 -0400

    content radio button removed from new response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(params[:user])
 		if @user.save
+			session[:user_id] = @user.id
 			@quiz = Quiz.new
 			render 'quizzes/new'
 		else 
