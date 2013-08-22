fa06bceaa94129dbf531858ea8d04730f0a2149b
Eric Iacutone <eric.iacutone@gmail.com>
2013-08-22 02:29:23 -0400

    analytics added

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'pg'
 gem "haml-rails"
 gem 'sass-rails', '~> 3.2.3'
 gem 'bootstrap-sass', '~> 2.3.2.0'
+gem 'google-analytics-rails'
 
 group :test do
 	gem 'rspec-rails'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ NewQuiz::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  GA.tracker = "UA-38341110-3"
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
